8be453fa94218946388bc45e31ab897e1f1d950a
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-06-25 16:34:14 -0700

    move button

--- src/routes/(nav)/pantry/DeeeeeeepLearning.svelte
+++ src/routes/(nav)/pantry/DeeeeeeepLearning.svelte
@@ -124,31 +124,6 @@
 	<canvas class="absolute top-0" bind:this={canvasEl} />
 
 	{#if pictureTaken}
-		<Button
-			outline={true}
-			on:click={() => {
-				pictureTaken = false;
-				canvasEl
-					.getContext('2d')
-					?.clearRect(0, 0, canvasEl.width, canvasEl.height);
-			}}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-				/>
-			</svg>
-		</Button>
-
 		{#if tableResults}
 			<Table>
 				<TableHead>
@@ -212,9 +187,35 @@
 					{/each}
 				</TableBody>
 			</Table>
-			{#if tableResults.length}
-				<Button on:click={addAll} class="self-end">Add all</Button>
-			{/if}
+			<div class="self-end flex flex-row items-center gap-4">
+				<Button
+					outline={true}
+					on:click={() => {
+						pictureTaken = false;
+						canvasEl
+							.getContext('2d')
+							?.clearRect(0, 0, canvasEl.width, canvasEl.height);
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+						/>
+					</svg>
+				</Button>
+				{#if tableResults.length}
+					<Button on:click={addAll}>Add all</Button>
+				{/if}
+			</div>
 		{/if}
 	{:else}
 		<Button on:click={doProcessingStuff}>
