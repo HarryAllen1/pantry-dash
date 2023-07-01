12e5ae538def1b67970e0c4329dc6c454cb14340
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2023-07-01 11:07:59 -0700

    Revert "slightly fix mobile"
    This reverts commit 5e8f0e33370f48abf23dfde356164672d96c90c9.

--- src/routes/(nav)/+page.svelte
+++ src/routes/(nav)/+page.svelte
@@ -34,10 +34,7 @@
 		placeholder="Filter results"
 	/>
 
-	<div
-		class:grid={filteredRecipes.length}
-		class="grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-4"
-	>
+	<div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-2 gap-4">
 		{#each filteredRecipes as recipe}
 			<RecipeCard
 				title={recipe.title}
